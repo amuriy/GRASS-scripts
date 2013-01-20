4971b8c942c5717ebc56cd804f96560ea1d56cfd
amuriy <amuriy@gmail.com>
2013-01-21 01:15:43 +0400

    v.ldm.py version 2.0

--- Python/v.ldm.py
+++ Python/v.ldm.py
@@ -11,6 +11,8 @@
 #               on the graphic monitor, save to vector line and update attribute table
 #               with LDM parameters.
 #
+# VERSION: 2.0 (rewritten in Python, works both in GRASS 6.4 and GRASS 7.0)
+#
 # COPYRIGHT:    (C) 2011-2013 Alexander Muriy / GRASS Development Team
 #
 #  This program is free software; you can redistribute it and/or modify
@@ -38,12 +40,22 @@
 #%  gisprompt: old,vector,vector
 #%End
 #%Option
+#%  key: type
+#%  type: string
+#%  required: no
+#%  multiple: no
+#%  options: orient,direct
+#%  description: Type of LDM graphics
+#% descriptions: orient; orientation only; direct; orientation and direction
+#%  answer: orient
+#%End
+#%Option
 #%  key: output
 #%  type: string
 #%  required: no
 #%  multiple: no
 #%  key_desc: name
-#%  description: Name of output vector map
+#%  description: Name of output vector map 
 #%  gisprompt: new,vector,vector
 #%End
 #%Option
@@ -71,22 +83,13 @@
 #%  gisprompt: new_file,file,output
 #%End
 #%Flag
-#%  key: n
-#%  description: Don't show LDM graphics on the screen
-#%End
-#%Flag
-#%  key: s 
-#%  description: Save LDM graphics to file (for use with d.graph)
-#%End
-#%Flag
-#%  key: l
-#%  description: Save LDM line as vector map and create attribute table with LDM parameters
+#%  key: x
+#%  description: Show LDM graphics on the screen
 #%End
 #%Flag
 #%  key: g
 #%  description: Print in shell script style
 #%End
-
 
 import sys
 import os
@@ -106,7 +109,6 @@ except:
             print "You must be in GRASS GIS to run this program."
             sys.exit(1)
         
-
 def cleanup():
     inmap = options['map']
     nuldev = file(os.devnull, 'w')
@@ -114,6 +116,8 @@ def cleanup():
     for f in glob.glob(tmp + '*'):
         grass.try_remove(f)
     grass.run_command('g.remove', vect = 'v_ldm_vect', flags = 'f',
+                      quiet = True, stderr = nuldev)
+    grass.run_command('g.remove', vect = 'v_ldm_vect_cats', flags = 'f',
                       quiet = True, stderr = nuldev)
     grass.run_command('g.remove', vect = 'v_ldm_hull', flags = 'f',
                       quiet = True, stderr = nuldev)
@@ -134,11 +138,14 @@ def main():
     width = options['width']
     color = options['color']
     graph = options['graph']
-    
+    ldm_type = options['type']
+
     mapset = grass.gisenv()['MAPSET']
 
-    global tmp, nuldev
+    global tmp, nuldev, grass_version
     nuldev = None
+
+    grass_version = grass.version().version[0]
 
     # setup temporary files
     tmp = grass.tempfile()
@@ -151,25 +158,21 @@ def main():
     iflines = grass.vector_info_topo(inmap)['lines']
     if iflines == 0:
         grass.fatal(_("<%s> does not exist.") % inmap)
-
-    # check for flags and options
-    if not flags['s'] and graph:
-        grass.fatal(_("Please use flag <-s> to save LDM graphics in file <%s> ") % graph)
-
-    if flags['s'] and not graph:
-        grass.fatal(_("Please specify \"graph\" file to save LDM graphics")) 
-
-    if not flags['l'] and output:
-        grass.fatal(_("Please use flag <-l> to save LDM line to \"%s\" vector map ") % output)
-
-    if flags['l'] and not output:
-        grass.fatal(_("Please specify \"output\" vector map to save LDM line"))
-        
     
+    # check for options
+    if flags['x']:
+        env = grass.gisenv()
+        mon = env.get('MONITOR', None)
+        if not mon:
+            grass.fatal(_("No monitor selected. Run <d.mon> to select monitor."))
+
     ####### DO IT #######
     # copy input vector map and drop table
     grass.run_command('g.copy', vect = (inmap, 'v_ldm_vect'), quiet = True, stderr = nuldev)
-    grass.run_command('v.db.droptable', map = 'v_ldm_vect', flags = 'f', quiet = True, stderr = nuldev)
+    db = grass.vector_db('v_ldm_vect', quiet = True, stderr = nuldev) 
+    if db != {}:
+        grass.run_command('v.db.droptable', _map = 'v_ldm_vect', flags = 'f', quiet = True, stderr = nuldev)
+
     inmap = 'v_ldm_vect'
 
     # make convex hull around lines
@@ -190,15 +193,20 @@ def main():
     count = grass.vector_info_topo(inmap)['lines']
 
     # add temp table with azimuths and lengths of lines
-    grass.run_command('v.db.addtable', _map = inmap, table = 'tmp_tab', 
+    in_cats = inmap + '_cats'    
+    grass.run_command('v.category', _input = inmap, option = 'add', 
+                      output = in_cats, quiet = True, stderr = nuldev)
+    grass.run_command('v.db.addtable', _map = in_cats, table = 'tmp_tab', 
                       columns = 'sum_azim double, len double', quiet = True, stderr = nuldev)
-    grass.run_command('v.to.db', _map = inmap, opt = 'azimuth', 
+    grass.run_command('v.db.connect', _map = in_cats, table = 'tmp_tab', 
+                      flags = 'o', quiet = True, stderr = nuldev)
+    grass.run_command('v.to.db', _map = in_cats, opt = 'azimuth', 
                       columns = 'sum_azim', units = 'radians', quiet = True, stderr = nuldev)
-    grass.run_command('v.to.db', _map = inmap, opt = 'length',  
+    grass.run_command('v.to.db', _map = in_cats, opt = 'length',  
                       columns = 'len', units = 'meters', quiet = True, stderr = nuldev)    
 
     # find end azimuth
-    p = grass.pipe_command('v.db.select', _map = inmap, columns = 'sum_azim', flags = 'c', quiet = True)
+    p = grass.pipe_command('v.db.select', _map = in_cats, columns = 'sum_azim', flags = 'c', quiet = True, stderr = nuldev)
     c = p.communicate()[0].strip().split('\n')
 
     sin = []
@@ -259,8 +267,8 @@ def main():
     start_azim = 180 - end_azim
     start_azim_dms = decimal2dms(start_azim)
     
-    p = grass.pipe_command('v.db.select', _map = inmap, columns = 'len',
-                           flags = 'c', quiet = True)
+    p = grass.pipe_command('v.db.select', _map = in_cats, columns = 'len',
+                           flags = 'c', quiet = True, stderr = nuldev)
     c = p.communicate()[0].strip().split('\n')
 
     mean_length = sum(map(float,c))/len(c)
@@ -283,11 +291,12 @@ def main():
                                       coord = center_coords, quiet = True).strip()
 
     # make "arrowhead" symbol
-    if not flags['n']:
+    if flags['x'] or graph:
         tmp3 = tmp + '.arrowhead_1'
         outf3 = file(tmp3, 'w')
-        
-        t1 = """VERSION 1.0
+
+        if ldm_type == 'direct':
+            t1 = """VERSION 1.0
 BOX -0.5 -0.5 0.5 0.5
 POLYGON
   RING
@@ -307,32 +316,37 @@ POLYGON
   END
 END
 """
-        outf3.write(t1)
-        outf3.close()
-        
-        gisdbase = grass.gisenv()['GISDBASE']
-        location = grass.gisenv()['LOCATION_NAME']
-        mapset = grass.gisenv()['MAPSET']
-        symbols_dir = os.path.join(gisdbase, location, mapset, 'symbol', 'arrows')
-        symbol = os.path.join(symbols_dir, 'arrowhead_1')
+            outf3.write(t1)
+            outf3.close()
     
-        if not os.path.exists(symbols_dir):
-            try:
-                os.makedirs(symbols_dir)
-            except OSError:
-                pass
+            gisdbase = grass.gisenv()['GISDBASE']
+            location = grass.gisenv()['LOCATION_NAME']
+            mapset = grass.gisenv()['MAPSET']
+            symbols_dir = os.path.join(gisdbase, location, mapset, 'symbol', 'arrows')
+            symbol = os.path.join(symbols_dir, 'arrowhead_1')
+    
+            if not os.path.exists(symbols_dir):
+                try:
+                    os.makedirs(symbols_dir)
+                except OSError:
+                    pass
         
-        if not os.path.isfile(symbol):
-            shutil.copyfile(tmp3, symbol)
+            if not os.path.isfile(symbol):
+                shutil.copyfile(tmp3, symbol)
 
     
-        # write LDM graph file and display line of LDM with the arrow
-        tmp4 = tmp + '.ldm'
-        outf4 = file(tmp4, 'w')
-
-        arrow_size = int(width) * 7
-        arrow_azim = 360 - float(end_azim)
+        # write LDM graph file and optionally display line of LDM with an arrow
+    tmp4 = tmp + '.ldm'
+    outf4 = file(tmp4, 'w')
     
+    if grass_version == '6':
+        arrow_size = int(width) * 6
+    elif grass_version == '7':
+        arrow_size = int(width) * 1.3
+
+    arrow_azim = 360 - float(end_azim)
+
+    if ldm_type == 'direct':
         t2 = string.Template("""
 move $start_coords
 width $width
@@ -343,23 +357,36 @@ rotation $arrow_azim
 width $width
 symbol $symbol_s $arrow_size $end_coords $color
 """)    
-
         s2 = t2.substitute(start_coords = start_coords, width = width, color = color,
-                           end_coords = end_coords, arrow_azim = arrow_azim,
-                           symbol_s = "arrows/arrowhead_1", arrow_size = arrow_size)
+                       end_coords = end_coords, arrow_azim = arrow_azim,
+                       symbol_s = "arrows/arrowhead_1", arrow_size = arrow_size)
+    else:
+        t2 = string.Template("""
+move $start_coords
+width $width
+color $color
+draw $end_coords
+""")    
+        s2 = t2.substitute(start_coords = start_coords, width = width, color = color,
+                       end_coords = end_coords)
 
-        outf4.write(s2)
-        outf4.close()
+    outf4.write(s2)
+    outf4.close()
 
-        if flags['s'] and graph:
-            shutil.copyfile(tmp4, graph)
+    if graph:
+        shutil.copyfile(tmp4, graph)
+
+
+
+    if flags['x']:
+        if graph:
             grass.run_command('d.graph', _input = graph, flags = 'm', quiet = True, stderr = nuldev)
         else:
             grass.run_command('d.graph', _input = tmp4, flags = 'm', quiet = True, stderr = nuldev)
 
 
-    # save LDM line to vector if flag "-l" set
-    if flags['l'] and output:
+    # save LDM line to vector if option "output" set  
+    if output:
         tmp5 = tmp + '.line'
         outf5 = file(tmp5, 'w')
 
@@ -368,20 +395,25 @@ symbol $symbol_s $arrow_size $end_coords $color
 
         outf5.close()
 
-        grass.run_command('v.in.lines', _input = tmp5, output = output,
-                          fs = " ", overwrite = True, quiet = True)
+        if grass_version == '6':
+            grass.run_command('v.in.lines', _input = tmp5, output = output,
+                              fs = " ", overwrite = True, quiet = True)
+        elif grass_version == '7':
+            grass.run_command('v.in.lines', _input = tmp5, output = output,
+                              separator = " ", overwrite = True, quiet = True)
+
         out_cats = output + '_cats'
-        grass.run_command('v.category', _input = output, output = out_cats, quiet = True)
-        grass.run_command('g.rename', vect = (out_cats,output), overwrite = True,
-                          quiet = True, stderr = nuldev)
+        grass.run_command('v.category', _input = output, option = 'add', 
+                          output = out_cats, quiet = True, stderr = nuldev)
+        grass.run_command('g.rename', vect = (out_cats,output), 
+                          overwrite = True, quiet = True, stderr = nuldev)
         
         if circ_var:
-            col = "CompassA double,DirMean double,CirVar double,AveX double,AveY double,AveLen double"
+            col = 'CompassA double,DirMean double,CirVar double,AveX double,AveY double,AveLen double'
         else:
-            col = "CompassA double,DirMean double,AveX double,AveY double,AveLen double"
-
+            col = 'CompassA double,DirMean double,AveX double,AveY double,AveLen double'
             
-        grass.run_command('v.db.addtable', _map = output, columns = col, quiet = True)
+        grass.run_command('v.db.addtable', _map = output, columns = col, quiet = True, stderr = nuldev)
 
         tmp6 = tmp + '.sql'
         outf6 = file(tmp6, 'w')
@@ -407,8 +439,7 @@ UPDATE $output SET AveLen = $mean_length;
 
 
     # print LDM parameters to stdout (with <-g> flag in shell style):
-    print_out = ['Compass Angle', 'Directional Mean', 'Average Center',
-                 'Average Length']
+    print_out = ['Compass Angle', 'Directional Mean', 'Average Center', 'Average Length']
     if circ_var:
         print_out.append('Circular Variance')
         
