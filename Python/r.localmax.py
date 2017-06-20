f3f89913435cf9a338e64b8e005e980c1091b0b0
amuriy <amuriy@gmail.com>
2017-06-20 13:23:37 +0300

    <r.localmax.py> added

--- Python/r.localmax.py
+++ Python/r.localmax.py
@@ -8,11 +8,11 @@
 #               (Institute of Environmental Geoscience, Moscow, Russia)  
 #               e-mail: amuriy AT gmail DOT com
 #
-#               Jachym Cepicky (jachym.cepicky AT centrum.cz) - original Perl version
+#               Idea by Jachym Cepicky - Perl version (local_max.pl)
 #
-# PURPOSE:      Local maxima filter for rasters
+# PURPOSE:      Local maxima filter for rasters (optionally saving to points)
 #
-# COPYRIGHT:    2012 Alexander Muriy / GRASS Development Team
+# COPYRIGHT:    2017, Alexander Muriy / GRASS Development Team
 #               
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -25,8 +25,9 @@
 #  GNU General Public License for more details.
 #
 ############################################################################
+
 #%Module 
-#%  description: Local maxima filter for rasters 
+#%  description: Local maxima filter for rasters (with optionally saving to points)
 #%  keywords: raster, map algebra
 #%End
 #%Option
@@ -46,17 +47,24 @@
 #%  gisprompt: new,cell,raster
 #%End
 #%Option
-#% key: size
+#%  key: points
+#%  type: string
+#%  required: no
+#%  key_desc: name
+#%  description: Name of output point map
+#%  gisprompt: new,vector,vector
+#%End
+#%Option
+#% key: msize
 #% type: integer
-#% required: no
-#% multiple: no
-#% description: Matrix size
-#% answer: 3
+#% required: yes
+#% description: Matrix size (odd number)
+#% answer: 11
 #%End
 ############################################################################
 
 import sys
-import os 
+import os
 import atexit
 
 try:
@@ -69,13 +77,81 @@ except:
             print "You must be in GRASS GIS to run this program."
             sys.exit(1)
 
+def cleanup():
+    nuldev = file(os.devnull, 'w')
+    grass.run_command('g.remove', type_ = 'rast,vect', pattern = 'R_LOCALMAX*', flags = 'f',
+                      quiet = True, stderr = nuldev)
 
+def main():
+    inr = options['input']
+    outr = options['output']
+    outp = options['points']
+    msize = options['msize']
+    msize = int(msize)
+    
+    global nuldev
+    nuldev = None
 
+    mmax = (msize-1)/2
 
+    mapcalc_str1 = "%s = if(%s == max(" % (outr, inr)
+    mapcalc_list = []
 
+    i = 0
+    j = 0
+    for x in xrange(i, msize):
+        for y in xrange(j, msize):
+            mapcalc_list.append("%s[%d,%d]" % (inr, -(mmax)+x, -(mmax)+y))
+    mapcalc_str2 = ','.join(mapcalc_list)
+    mapcalc_str3 = mapcalc_str1 + mapcalc_str2 + ("),%s,null())" % inr)
 
+    grass.mapcalc(mapcalc_str3)
+    
+    if outp:
+        vect1 = 'R_LOCALMAX_vect1'
+        grass.run_command('r.to.vect',
+                        input_ = outr, output = vect1,
+                        type_ = 'point',
+                        quiet = True, stderr = nuldev)
+        res = grass.raster_info(inr)['nsres']
+        vect2 = 'R_LOCALMAX_vect2'
+        grass.run_command('v.buffer',
+                        input_ = vect1, output = vect2,
+                        dist = res*2,
+                        quiet = True, stderr = nuldev)
+        vect3 = 'R_LOCALMAX_vect3'
+        grass.run_command('v.type',
+                          input_ = vect2, output = vect3,
+                          from_type_ = 'centroid',
+                          to_type = 'point',
+                          quiet = True, stderr = nuldev)
+        vect4 = 'R_LOCALMAX_vect4'
+        grass.run_command('v.category',
+                          input_ = vect3, opt = 'del', 
+                          output = vect4,
+                          quiet = True, stderr = nuldev)
+        vect5 = 'R_LOCALMAX_vect5'
+        grass.run_command('v.category',
+                          input_ = vect4, opt = 'add', 
+                          output = vect5,
+                          quiet = True, stderr = nuldev)
+        grass.run_command('v.db.addtable',
+                          map_ = vect5, columns = 'dist double',
+                          quiet = True, stderr = nuldev)
+        vect6 = 'R_LOCALMAX_vect6'
+        grass.run_command('v.distance',
+                          from_ = vect5, from_type = 'point',
+                          to = vect1, to_type = 'point',
+                          output = vect6,  upload = 'dist', column = 'dist',
+                          quiet = True, stderr = nuldev)
+        grass.run_command('v.select',
+                          ainput = vect1, binput = vect6,
+                          output = outp,
+                          quiet = True, stderr = nuldev)
 
+        return 0
+        
 if __name__ == "__main__":
     options, flags = grass.parser()
     atexit.register(cleanup)
-    sys.exit(main())        
+    sys.exit(main())
