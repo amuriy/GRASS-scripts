056a61d40378b3359233a5785ac7a8d8666b64c3
Alexander Muriy <amuriy@gmail.com>
2020-02-15 10:39:47 +0300

    upd

--- Python/v.triangle.py
+++ Python/v.triangle.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 ############################################################################
 #
@@ -106,7 +106,7 @@ except:
         from grass.script import core as grass
     except:
         if not os.environ.has_key("GISBASE"):
-            print "You must be in GRASS GIS to run this program."
+            print("You must be in GRASS GIS to run this program.")
             sys.exit(1)
 
 from grass.lib.gis    import *
